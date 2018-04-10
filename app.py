3396eb9007d5e624053e5dc1f2fbf215a65077ab
Jesse Stippel <jesse.stippel@operun.de>
2018-04-10 13:29:38 +0200

    added styling files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import pylinkvalidator
 from flask import Flask, render_template
+from pylinkvalidator import api
 
 app = Flask(__name__)
 app.config.from_object(__name__)
@@ -9,4 +9,22 @@ app.config.from_object(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    data = {
+        'error_pages': return_error_pages(),
+    }
+    return render_template('index.html', **data)
+
+
+def return_error_pages(site_links=['https://www.operun.de']):
+    crawled_site = api.crawl_with_options(
+        site_links,
+        {
+            'run-once': True,
+            'workers': 4,
+        },
+    )
+    error_pages = crawled_site.error_pages
+    error_links = []
+    for item in error_pages:
+        error_links.append(item.geturl())
+    return error_links
