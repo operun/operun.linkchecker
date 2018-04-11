4422dcd43604729f91e98c92eb6d82216285ccfd
JohannesNeumayr <johannes.neumayr@gmx.de>
2018-04-11 16:36:00 +0200

    basic structure

--- app.py
+++ app.py
@@ -1,30 +1,47 @@
 # -*- coding: utf-8 -*-
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from pylinkvalidator import api
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    data = {
-        'error_pages': return_error_pages(),
-    }
+    data = {}
+
+    form = request.form
+    if len(form):
+        website_url = form.get('website_url')
+        timeout = int(form.get('timeout'))
+        depth = int(form.get('depth'))
+        result = return_error_pages(
+            site_links=[website_url],
+            config={
+                'depth': depth,
+                'timeout': timeout,
+            },
+        )
+        data['bad_links'] = result
     return render_template('index.html', **data)
 
-
-def return_error_pages(site_links=['https://www.operun.de']):
+def return_error_pages(site_links=[], config={}):
     crawled_site = api.crawl_with_options(
         site_links,
-        {
-            'run-once': True,
-            'workers': 4,
-        },
+        config,
     )
     error_pages = crawled_site.error_pages
-    error_links = []
+    error_items = []
     for item in error_pages:
-        error_links.append(item.geturl())
-    return error_links
+        raw = error_pages[item]
+        item_url = raw.url_split.geturl()
+        item_status = raw.status()
+        item_message = raw.get_status_message()
+        data = {
+            'item_url': item_url,
+            'item_status': item_status,
+            'item_message': item_message,
+        }
+        error_items.append(data)
+    return error_items
