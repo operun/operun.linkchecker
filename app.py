b7a749c8d744488f562849e6efb7e129ce023533
Jesse Stippel <jesse.stippel@operun.de>
2018-04-12 09:42:06 +0200

    added spinner icon and some content margins

--- app.py
+++ app.py
@@ -10,12 +10,11 @@ app.config.from_object(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data = {}
-
     form = request.form
     if len(form):
-        website_url = form.get('website_url')
-        timeout = int(form.get('timeout'))
-        depth = int(form.get('depth'))
+        website_url = form.get('website_url', 'www.operun.de')
+        timeout = form.get('timeout', '')
+        depth = int(form.get('depth', ''))
         result = return_error_pages(
             site_links=[website_url],
             config={
@@ -23,25 +22,26 @@ def index():
                 'timeout': timeout,
             },
         )
-        data['bad_links'] = result
+        data['errors'] = result
     return render_template('index.html', **data)
 
+
 def return_error_pages(site_links=[], config={}):
+    error_items = []
     crawled_site = api.crawl_with_options(
         site_links,
         config,
     )
     error_pages = crawled_site.error_pages
-    error_items = []
     for item in error_pages:
         raw = error_pages[item]
         item_url = raw.url_split.geturl()
         item_status = raw.status
-        item_message = raw.get_status_message()
+        item_status_message = raw.get_status_message()
         data = {
             'item_url': item_url,
             'item_status': item_status,
-            'item_message': item_message,
+            'item_status_message': item_status_message,
         }
         error_items.append(data)
     return error_items
