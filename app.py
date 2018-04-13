2777fb14f0017324d5f044b7dc5a1b4a7e031bf6
Jesse Stippel <jesse.stippel@operun.de>
2018-04-13 16:50:13 +0200

    add conditions and code highlighting

--- app.py
+++ app.py
@@ -15,19 +15,18 @@ def index():
     template = 'index.html'
     form = request.form
     data = {}
-
     if len(form):
         submit = form.get('form.button.submit', '')
         website_url = form.get('website_url', '')
+        if not website_url:
+            return render_template(template)
         timeout = int(form.get('timeout', '5'))
         depth = int(form.get('depth', '0'))
         csv_data = form.get('csv_data', '')
-
         if submit == 'csv':
             if csv_data:
                 return csv_response(csv_data)
             return render_template(template)
-
         result = return_error_pages(
             site_links=[website_url],
             config={
@@ -35,15 +34,12 @@ def index():
                 'timeout': timeout,
             },
         )
-
         csv_data = generate_csv(result)
-
         data['errors'] = result
         data['website_url'] = website_url
         data['timeout'] = timeout
         data['depth'] = depth
         data['csv_data'] = csv_data
-
     return render_template(template, **data)
 
 
@@ -56,10 +52,22 @@ def return_error_pages(site_links=[], config={}):
     error_pages = crawled_site.error_pages
     for item in error_pages:
         raw = error_pages[item]
+        sources = raw.sources
+        sources_data = []
+        for source in sources:
+            source_url = source.origin.geturl()
+            source_html = source.origin_str
+            source_data = {
+                'source_url': source_url,
+                'source_html': source_html,
+            }
+            sources_data.append(source_data)
         item_url = raw.url_split.geturl()
         item_status = raw.status
         item_status_message = raw.get_status_message()
         data = {
+            'sources': len(sources),
+            'sources_data': sources_data,
             'item_url': item_url,
             'item_status': item_status,
             'item_status_message': item_status_message,
